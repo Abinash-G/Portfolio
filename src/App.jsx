2f09bece40e2fb2a3e0576b465b78acb91246a14
Abinash-G <abimadhu55555@gmail.com>
2025-12-03 09:10:49 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -273,7 +273,7 @@ const styles = `
 
 @keyframes mobilePop {
     0% { transform: scale(0.1); opacity: 0; }
-    80% { transform: scale(1.02); opacity: 1; }
+    80% { transform: scale(1.0); opacity: 1; }
     100% { transform: scale(1); opacity: 1; }
 }
 
